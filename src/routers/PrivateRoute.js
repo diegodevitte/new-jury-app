3a7f38b2014fafbb4864a7b6dfbc201e5d7f2384
diegodevitte <diegodevitte@gmail.com>
2022-07-25 12:40:33 -0300

    modificacion de rutas privadas

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,7 +3,7 @@ import { Redirect, Route, useLocation } from 'react-router-dom';
 import useAuth from '../auth/useAuth'
 import routes from "../helpers/routes";
 
-const PrivateRoute = ({ hasRole: role, ...props }) => {
+const PrivateRoute = ({ ...props }) => {
     const location = useLocation();
     const { isLogged } = useAuth();
 
